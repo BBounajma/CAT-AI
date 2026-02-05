1ecc5dfd9bfe221b52f165fe9bbde9667cce76aa
Bilal <bilal.bounajma@ethz.ch>
2026-02-05 11:24:00 +0700

    First cleaning

--- save_model_cpu.py
+++
@@ -1,9 +0,0 @@
-import torch
-import joblib
-from deeptab.models import SAINTClassifier
-
-model=joblib.load('saint_model.joblib')
-# Save the task_model's state_dict
-save_path = 'trained_saint.pth'
-torch.save(model.task_model.state_dict(), save_path)
-print(f"Task model saved to {save_path}.")
--- train_XGboost.py
+++ train_XGboost.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import sys
 import joblib
-import torch
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OrdinalEncoder
